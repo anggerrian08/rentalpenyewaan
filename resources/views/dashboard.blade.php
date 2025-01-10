ca758f9b715c9377636e9313d7e8a667131983e5
evvamaulani <emaulani087@gmail.com>
2025-01-10 09:29:30 +0700

    design navuser dan beranda

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.template')
 @section('content')
+    <br>
 
     <head>
         <!-- Header -->
@@ -23,7 +24,7 @@
                     <div class="card bg-overlay text-white border-0 shadow-sm">
                         <div class="card-img-overlay d-flex flex-column justify-content-center align-items-center">
                             <h5 class="card-title">Total Merek</h5>
-                            <p class="card-text">{{$total_merek}}</p>
+                            <p class="card-text">{{ $total_merek }}</p>
                         </div>
                         <img src="{{ asset('assets/images/logo/Card 13.svg') }}" alt="Background" class="card-img">
                     </div>
@@ -34,7 +35,7 @@
                     <div class="card bg-overlay text-white border-0 shadow-sm">
                         <div class="card-img-overlay d-flex flex-column justify-content-center align-items-center">
                             <h5 class="card-title">Total Mobil</h5>
-                            <p class="card-text">{{$total_car}}</p>
+                            <p class="card-text">{{ $total_car }}</p>
                         </div>
                         <img src="{{ asset('assets/images/logo/Card 12.svg') }}" alt="Background" class="card-img">
                     </div>
@@ -45,7 +46,7 @@
                     <div class="card bg-overlay text-white border-0 shadow-sm">
                         <div class="card-img-overlay d-flex flex-column justify-content-center align-items-center">
                             <h5 class="card-title">Total Transaksi</h5>
-                            <p class="card-text">{{$total_transaksi}}</p>
+                            <p class="card-text">{{ $total_transaksi }}</p>
                         </div>
                         <img src="{{ asset('assets/images/logo/Card 11.svg') }}" alt="Background" class="card-img">
                     </div>
@@ -54,21 +55,21 @@
         </div>
 
 
-            <!-- Bagian Chart -->
-            <div class="row g-4">
-                <div class="col-12 col-md-6">
-                    <div class="card border-0 shadow-sm p-3">
-                        <h5 class="text-center mb-3"></h5>
-                        {!! $chart->container() !!}
-                    </div>
-                </div>
-                <div class="col-12 col-md-6">
-                    <div class="card border-0 shadow-sm p-3">
-                        <h5 class="text-center mb-3"></h5>
-                        {!! $chartDonut->container() !!}
-                    </div>
+        <!-- Bagian Chart -->
+        <div class="row g-4">
+            <div class="col-12 col-md-6">
+                <div class="card border-0 shadow-sm p-3">
+                    <h5 class="text-center mb-3"></h5>
+                    {!! $chart->container() !!}
                 </div>
             </div>
+            <div class="col-12 col-md-6">
+                <div class="card border-0 shadow-sm p-3">
+                    <h5 class="text-center mb-3"></h5>
+                    {!! $chartDonut->container() !!}
+                </div>
+            </div>
+        </div>
         </div>
     </body>
 
