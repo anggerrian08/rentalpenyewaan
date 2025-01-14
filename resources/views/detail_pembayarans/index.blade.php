acb1302a0bd2703e1d418514d569f0ed9c35e2b7
labibul710 <labibulfirdaus3@gmail.com>
2025-01-14 14:56:55 +0700

    detail pembayaran

--- resources/views/detail_pembayarans/index.blade.php
+++ resources/views/detail_pembayarans/index.blade.php
@@ -101,51 +101,54 @@
             </div>
         </div>
 
-        <div class="col-sm-12 mt-3">
-            <div class="card-block row">
-                <div class="col-sm-12 col-lg-12 col-xl-12">
-                    <div class="table-responsive custom-scrollbar">
-                        <table class="table">
-                            <tr>
-                                <th>No</th>
-                                <th>Booking ID</th>
-                                <th>Email User</th>
-                                <th>Tanggal Pinjam</th>
-                                <th>Tanggal Kembali</th>
-                                <th>Durasi Rental (Hari)</th>
-                                <th>Total Harga</th>
-                                <th>Aksi</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                                @forelse ($data as $item)
-                                    <tr>
-                                        <td>{{ $loop->iteration }}</td>
-                                        <td>{{ $item->booking_id }}</td>
-                                        <td>{{ $item->booking->user->email ?? '-' }}</td> <!-- Email user -->
-                                        <td>{{ \Carbon\Carbon::parse($item->booking->order_date)->translatedFormat('d-M-Y') }}
-                                        </td>
-                                        <td>{{ \Carbon\Carbon::parse($item->booking->return_date)->translatedFormat('d-M-Y') ?? '-' }}
-                                        </td> <!-- Format tanggal kembali -->
-                                        <td>{{ $item->rental_duration_days }}</td>
-                                        <td>{{ number_format($item->total_price, 0, ',', '.') }}</td> <!-- Format harga -->
-                                        <td>
-                                            <button type="button" class="btn btn-info btn-sm"> <i class="bi bi-eye"></i>
-                                                Detail</button>
-                                        </td>
-                                    </tr>
-                                @empty
-                                    <tr>
-                                        <td colspan="8" class="text-center">
-                                            <img src="{{ asset('assets/images/logo/tidakada.png') }}" width="500px"
-                                                alt="">
-                                        </td>
-                                    </tr>
-                                @endforelse
-                            </tbody>
-                        </table>
+        <div class="card p-3">
+            <div class="col-sm-12 mt-3">
+                <div class="card-block row">
+                    <div class="col-sm-12 col-lg-12 col-xl-12">
+                        <div class="table-responsive custom-scrollbar">
+                            <table class="table">
+                                <tr>
+                                    <th>No</th>
+                                    <th>Booking ID</th>
+                                    <th>Email User</th>
+                                    <th>Tanggal Pinjam</th>
+                                    <th>Tanggal Kembali</th>
+                                    <th>Durasi Rental (Hari)</th>
+                                    <th>Total Harga</th>
+                                    <th>Aksi</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                    @forelse ($data as $item)
+                                        <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>{{ $item->booking_id }}</td>
+                                            <td>{{ $item->booking->user->email ?? '-' }}</td> <!-- Email user -->
+                                            <td>{{ \Carbon\Carbon::parse($item->booking->order_date)->translatedFormat('d-M-Y') }}
+                                            </td>
+                                            <td>{{ \Carbon\Carbon::parse($item->booking->return_date)->translatedFormat('d-M-Y') ?? '-' }}
+                                            </td> <!-- Format tanggal kembali -->
+                                            <td>{{ $item->rental_duration_days }}</td>
+                                            <td>{{ number_format($item->total_price, 0, ',', '.') }}</td> <!-- Format harga -->
+                                            <td>
+                                                <button type="button" class="btn btn-info btn-sm"> <i class="bi bi-eye"></i>
+                                                    Detail</button>
+                                            </td>
+                                        </tr>
+                                    @empty
+                                        <tr>
+                                            <td colspan="8" class="text-center">
+                                                <img src="{{ asset('assets/images/logo/tidakada.png') }}" width="500px"
+                                                    alt="">
+                                            </td>
+                                        </tr>
+                                    @endforelse
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
+
         </div>
     @endsection
